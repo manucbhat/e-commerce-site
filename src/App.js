66d8cad1090be5b0c777c3c8a19ca95683aa4eb1
manu <manucbhat608@gmail.com>
2022-12-08 18:32:54 +0530

    Added starting components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
-
+import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom";
+import Products from "./Components/Products";
+import CheckOutPage from "./Components/CheckOutPage";
 function App() {
   return (
-    <div className="App">    
+    <div>  
+  <Router>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+     
+        <header className="product">E-Commerce-Site</header>
+        <Link to={'/'} className="nav-link"><span id="products" className="product">Products</span></Link>
+        <Link to={'/Checkout'} className="nav-link"><img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" style={{width:"20px",height:"30px",marginLeft:"30px",marginRight:"50px"}} /></Link>
+      </nav>
+      
+      <Routes>
+        <Route exact path='/' element={<Products />} />
+        <Route exact path='/Checkout' element={<CheckOutPage />} />
+      </Routes>
+  </Router>
     </div>
   );
 }
