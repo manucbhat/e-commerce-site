a00c247adf22c37f90271c9896e1e1617d175ff4
manu <manucbhat608@gmail.com>
2022-12-09 13:27:47 +0530

    Added styles

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -9,17 +9,21 @@ function Products(){
             <div style={{display:"flex",justifyContent:"space-evenly",flexWrap:"wrap"}}>
             <FilterBox />
         {ItemsData.map(product => {
+
               return (
                 <div key={product.id} style={{display:"flex",justifyContent:"space-evenly",marginTop:"60px",flexWrap:"wrap"}}>
-               <img src={product.imageURL} alt="image" style={{width:"200px",height:"200px"}} /> 
-               <br />
-                  <h3 style={{float:"left",position:"relative",maxWidth:"200px",overflow:"hidden",margin:"0px",textAlign:"start"}}>{ product.name}</h3>
+                <div className="sub-div1">
+               <img src={product.imageURL} alt="image" style={{width:"300px",height:"200px",display:"flex"}} /> 
+            <div>
+                  <h3 >{ product.name}</h3>
                   <br />
-                  <p style={{display:"flex",float:"left",position:"absolute", margin:"0px"}}>{ product.category }</p>
-                  <p style={{display:"flex",position:"absolute", margin:"0px"}}>Rs.{ product.price }</p>
-                  <p>
-                    <button>Add to Cart</button>
+                  <p >{ product.category }</p>
+                  <p >Rs.{ product.price }</p>
+                  <p>{(product.available !== 0) ? <><label style={{marginRight:"10px",color:"green"}}>In Stock</label>
+                    <button>Add to Cart</button></> : <><label style={{marginRight:"10px",color:"red"}}>Out Of Stock</label><button style={{color:"red"}}>Notify Me</button></>}
                   </p>
+                  </div>
+                  </div>
                 </div>
               );
             })}
